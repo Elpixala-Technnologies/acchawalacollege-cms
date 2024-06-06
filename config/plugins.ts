fc149530f398afc1496e36deac438730b76fcc7f
elPixalaTechnologies <146587287+elPixalaTechnologies@users.noreply.github.com>
2024-06-06 15:55:27 +0530

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,17 @@
 module.exports = ({ env }) => ({
     graphql: {
-      enabled: true,
-      config: {
-        endpoint: "/graphql",
-        generateArtifacts: true,
-        artifacts: {
-          schema: true,
-          typegen: true,
-        },
-        shadowCRUD: true,
-        playgroundAlways: true,
-        depthLimit: 9,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
-        },
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
       },
     },
+  },
     upload: {
       config: {
         provider: "strapi-provider-upload-do",
